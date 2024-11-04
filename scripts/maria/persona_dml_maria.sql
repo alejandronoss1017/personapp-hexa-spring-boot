cc61473298d0ad5f6565ac0e88a54f6fd2bf868e
Carlos Santiago Rojas Calderón <carlosantirc@gmail.com>
2024-11-04 16:55:25 -0500

    ✨ Dmls for dbs

--- scripts/maria/persona_dml_maria.sql
+++ scripts/maria/persona_dml_maria.sql
@@ -6,3 +6,30 @@ VALUES
 	(321654987,'Pepa','Juarez','F',30),
 	(147258369,'Pepita','Juarez','F',10),
 	(963852741,'Fede','Perez','M',18);
+
+INSERT INTO 
+    `persona_db`.`profesion`(`id`, `nom`, `des`) 
+VALUES
+    (1, 'Ingeniero de Software', 'Profesional especializado en desarrollo de software'),
+    (2, 'Médico General', 'Profesional de la salud especializado en medicina general'),
+    (3, 'Profesor', 'Profesional dedicado a la enseñanza'),
+    (4, 'Arquitecto', 'Profesional especializado en diseño y construcción'),
+    (5, 'Contador', 'Profesional especializado en contabilidad y finanzas');
+
+INSERT INTO
+    `persona_db`.`telefono`(`num`, `oper`, `duenio`)
+VALUES
+    ('3001234567', 'Claro', 123456789),    -- Teléfono de Pepe
+    ('3009876543', 'Movistar', 987654321), -- Teléfono de Pepito
+    ('3002345678', 'Tigo', 321654987),     -- Teléfono de Pepa
+    ('3007654321', 'Claro', 147258369),    -- Teléfono de Pepita
+    ('3005678901', 'Movistar', 963852741); -- Teléfono de Fede
+
+INSERT INTO
+    `persona_db`.`estudios`(`id_prof`, `cc_per`, `fecha`, `univer`)
+VALUES
+    (1, 123456789, '2015-06-30', 'Universidad Nacional'),    -- Pepe es Ingeniero
+    (2, 987654321, '2016-07-15', 'Universidad de Los Andes'), -- Pepito es Médico
+    (3, 321654987, '2015-12-20', 'Universidad Javeriana'),   -- Pepa es Profesora
+    (1, 963852741, '2023-06-30', 'Universidad Nacional'),    -- Fede es Ingeniero
+    (5, 123456789, '2018-06-30', 'Universidad Nacional');    -- Pepe también es Contador
--- scripts/mongo/persona_dml_mongo.js
+++ scripts/mongo/persona_dml_mongo.js
@@ -44,3 +44,110 @@ db.persona.insertMany([
 		"_class": "co.edu.javeriana.as.personapp.mongo.document.PersonaDocument"
 	}
 ], { ordered: false });
+
+// Insert professions
+db.profesion.insertMany([
+    {
+        "_id": NumberInt(1),
+        "nom": "Ingeniero de Software",
+        "des": "Profesional especializado en desarrollo de software",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.ProfesionDocument"
+    },
+    {
+        "_id": NumberInt(2),
+        "nom": "Médico General",
+        "des": "Profesional de la salud especializado en medicina general",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.ProfesionDocument"
+    },
+    {
+        "_id": NumberInt(3),
+        "nom": "Profesor",
+        "des": "Profesional dedicado a la enseñanza",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.ProfesionDocument"
+    },
+    {
+        "_id": NumberInt(4),
+        "nom": "Arquitecto",
+        "des": "Profesional especializado en diseño y construcción",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.ProfesionDocument"
+    },
+    {
+        "_id": NumberInt(5),
+        "nom": "Contador",
+        "des": "Profesional especializado en contabilidad y finanzas",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.ProfesionDocument"
+    }
+], { ordered: false });
+
+// Insert phone numbers
+db.telefono.insertMany([
+    {
+        "_id": "3001234567",
+        "oper": "Claro",
+        "duenio": NumberInt(123456789),
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.TelefonoDocument"
+    },
+    {
+        "_id": "3009876543",
+        "oper": "Movistar",
+        "duenio": NumberInt(987654321),
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.TelefonoDocument"
+    },
+    {
+        "_id": "3002345678",
+        "oper": "Tigo",
+        "duenio": NumberInt(321654987),
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.TelefonoDocument"
+    },
+    {
+        "_id": "3007654321",
+        "oper": "Claro",
+        "duenio": NumberInt(147258369),
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.TelefonoDocument"
+    },
+    {
+        "_id": "3005678901",
+        "oper": "Movistar",
+        "duenio": NumberInt(963852741),
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.TelefonoDocument"
+    }
+], { ordered: false });
+
+// Insert studies
+db.estudios.insertMany([
+    {
+        "id_prof": NumberInt(1),
+        "cc_per": NumberInt(123456789),
+        "fecha": new Date("2015-06-30"),
+        "univer": "Universidad Nacional",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.EstudiosDocument"
+    },
+    {
+        "id_prof": NumberInt(2),
+        "cc_per": NumberInt(987654321),
+        "fecha": new Date("2016-07-15"),
+        "univer": "Universidad de Los Andes",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.EstudiosDocument"
+    },
+    {
+        "id_prof": NumberInt(3),
+        "cc_per": NumberInt(321654987),
+        "fecha": new Date("2015-12-20"),
+        "univer": "Universidad Javeriana",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.EstudiosDocument"
+    },
+    {
+        "id_prof": NumberInt(1),
+        "cc_per": NumberInt(963852741),
+        "fecha": new Date("2023-06-30"),
+        "univer": "Universidad Nacional",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.EstudiosDocument"
+    },
+    {
+        "id_prof": NumberInt(5),
+        "cc_per": NumberInt(123456789),
+        "fecha": new Date("2018-06-30"),
+        "univer": "Universidad Nacional",
+        "_class": "co.edu.javeriana.as.personapp.mongo.document.EstudiosDocument"
+    }
+], { ordered: false });
